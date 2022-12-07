e6f953d6aaf011e119404700a262eee21107d0fd
Aadarsh <aadarsh/hegde@gmail.com>
2022-12-07 10:56:48 -0600

    Final Frontend Updates

--- crimeMap/crimeMap/Form1.Designer.cs
+++ crimeMap/crimeMap/Form1.Designer.cs
@@ -29,22 +29,14 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.search_data = new System.Windows.Forms.Button();
-            this.txt_city = new System.Windows.Forms.Label();
-            this.txt_street = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.loadCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.loadCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -62,17 +54,10 @@
             // 
             this.splitContainer1.Panel1.Controls.Add(this.button2);
             this.splitContainer1.Panel1.Controls.Add(this.label3);
-            this.splitContainer1.Panel1.Controls.Add(this.label2);
-            this.splitContainer1.Panel1.Controls.Add(this.textBox4);
-            this.splitContainer1.Panel1.Controls.Add(this.label1);
-            this.splitContainer1.Panel1.Controls.Add(this.textBox3);
             this.splitContainer1.Panel1.Controls.Add(this.button1);
             this.splitContainer1.Panel1.Controls.Add(this.search_data);
-            this.splitContainer1.Panel1.Controls.Add(this.txt_city);
-            this.splitContainer1.Panel1.Controls.Add(this.txt_street);
-            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
-            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
             this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
             // 
             // splitContainer1.Panel2
             // 
@@ -81,47 +66,43 @@
             this.splitContainer1.SplitterDistance = 369;
             this.splitContainer1.TabIndex = 0;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "See Static Crime Map";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "UIUC Crime Scanner";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "See Latest Crime";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // search_data
             // 
-            this.search_data.Location = new System.Drawing.Point(67, 314);
+            this.search_data.Location = new System.Drawing.Point(118, 286);
             this.search_data.Name = "search_data";
             this.search_data.Size = new System.Drawing.Size(120, 23);
             this.search_data.TabIndex = 4;
             this.search_data.Text = "Get Safest Directions";
             this.search_data.UseVisualStyleBackColor = true;
             this.search_data.Click += new System.EventHandler(this.search_data_Click);
-            // 
-            // txt_city
-            // 
-            this.txt_city.AutoSize = true;
-            this.txt_city.Location = new System.Drawing.Point(42, 187);
-            this.txt_city.Name = "txt_city";
-            this.txt_city.Size = new System.Drawing.Size(24, 13);
-            this.txt_city.TabIndex = 3;
-            this.txt_city.Text = "City";
-            // 
-            // txt_street
-            // 
-            this.txt_street.AutoSize = true;
-            this.txt_street.Location = new System.Drawing.Point(31, 155);
-            this.txt_street.Name = "txt_street";
-            this.txt_street.Size = new System.Drawing.Size(35, 13);
-            this.txt_street.TabIndex = 2;
-            this.txt_street.Text = "Street";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(76, 184);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(76, 149);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
             // 
             // menuStrip1
             // 
@@ -141,12 +122,6 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // loadCSVToolStripMenuItem
-            // 
-            this.loadCSVToolStripMenuItem.Name = "loadCSVToolStripMenuItem";
-            this.loadCSVToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.loadCSVToolStripMenuItem.Text = "Update CSV";
-            // 
             // webBrowser1
             // 
             this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -156,64 +131,11 @@
             this.webBrowser1.Size = new System.Drawing.Size(737, 622);
             this.webBrowser1.TabIndex = 0;
             // 
-            // button1
+            // loadCSVToolStripMenuItem
             // 
-            this.button1.Location = new System.Drawing.Point(67, 106);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "See Latest Crime";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(76, 219);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 226);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Lat";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(76, 256);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 9;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 263);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Long";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(73, 90);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(105, 13);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "UIUC Crime Scanner";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(67, 343);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(120, 23);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "See Static Crime Map";
-            this.button2.UseVisualStyleBackColor = true;
+            this.loadCSVToolStripMenuItem.Name = "loadCSVToolStripMenuItem";
+            this.loadCSVToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadCSVToolStripMenuItem.Text = "Update CSV";
             // 
             // Form1
             // 
@@ -238,22 +160,14 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.Label txt_street;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.WebBrowser webBrowser1;
-        private System.Windows.Forms.Label txt_city;
         private System.Windows.Forms.Button search_data;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem loadCSVToolStripMenuItem;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem loadCSVToolStripMenuItem;
     }
 }
 
--- crimeMap/crimeMap/Form1.cs
+++ crimeMap/crimeMap/Form1.cs
@@ -5,9 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 //All controls relating to front end
 namespace crimeMap
@@ -27,27 +28,75 @@ namespace crimeMap
 
         private void search_data_Click(object sender, EventArgs e)
         {
+            
+            try
+            {
+                StringBuilder queryaddress = new StringBuilder();
+                queryaddress.Append("http://maps.google.com/"); //Query google maps
+                
+                webBrowser1.Navigate(queryaddress.ToString());
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+            
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        private void button2_Click(object sender, EventArgs e) //Display open layers content
+        {
+            /*
+            OpenFileDialog od = new OpenFileDialog();
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                System.Diagnostics.Process proc = System.Diagnostics.Process.Start(od.FileName);
+                proc.WaitForInputIdle();
+
+                while (proc.MainWindowHandle == IntPtr.Zero)
+                {
+                    System.Threading.Thread.Sleep(100);
+                    proc.Refresh();
+                }
+                SetParent(proc.MainWindowHandle, this.Handle);
+            }
+            */
+            System.Diagnostics.Process proc = System.Diagnostics.Process.Start("C:\\Users\\Aadarsh Hegde\\Documents\\College Files\\CrimeRating\\course-project-group-61\\map_api\\openlayers.html");
+            //Console.WriteLine(@"file://C:\openlayers.html");
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            /*
             string street = txt_street.Text;
             string city = txt_city.Text;
             try
             {
                 StringBuilder queryaddress = new StringBuilder();
                 queryaddress.Append("http://maps.google.com/"); //Query google maps
-                /*
+                
                 if (street != string.Empty)
                 {
-                    queryaddress.Append(street + ",+");
+                    queryaddress.Append(street + ",+IL");
                 }
                 if (city != string.Empty)
                 {
                     queryaddress.Append(city);
                 }
-                */
+                
                 webBrowser1.Navigate(queryaddress.ToString());
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message.ToString(), "Error");
             }
+            */
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
